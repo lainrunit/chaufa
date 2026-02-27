d5790eabba5a3d594b283f7131613ce0dd295830
Alex-Dev-22 <aledevco@gmail.com>
2026-02-27 11:50:51 -0300

    sex

--- src/camry.c
+++ src/camry.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+
 #define VERSION "0.1v"
 
-void 
-optionhelp() {
+void optionhelp() {
   printf("Uso de Camry: \n");
   printf("-h --help   Despliega este mensaje. \n");
   printf("-v --version  Muestra la versión de Camry. \n");
 }
 
-int 
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
 	int c;
 
-	while (1) {
+  static struct option long_options[] =
+  {
+			{"help", no_argument, 0, 'h'},
+			{"version", no_argument, 0, 'v'},
+			{0, 0, 0, 0}
+	};
 
-		int option_index = 0;
 
-		static struct option long_options[] = {
-				{"help"		, no_argument, 0, 'h'},
-				{"version", no_argument, 0, 'v'},
-				{0, 			0, 			0, 				0}
-				};
-  
-		c = getopt_long(argc, argv, "hv",
-									long_options, &option_index);
-	
-		if (c == -1)
-				break;
-
-		switch (c) {
+	while (1) 
+  {
+    if( (c = getopt_long(argc, argv, "hv", long_options, 0)) == -1) break;
+		switch (c) 
+    {
 			case 'h':
 				optionhelp();
 				break;
@@ -44,11 +39,11 @@ main(int argc, char *argv[]) {
 				break;
 		}
 	}
-			
-	if (argc == 1 || optind == 1) {
+	
+	if (argc == 1 || optind == 1)
+  {
       printf("No se encontraron argumentos. (usa --help para ayuda).\n");
       exit(EXIT_FAILURE);
-	exit(EXIT_SUCCESS);
+	    exit(EXIT_SUCCESS);
 	}
-
 }
