9d928dfe7f054ced3609e4ab6e863905c7edb027
lainrunit <asahiminanesa@tutamail.com>
2026-02-27 09:37:04 -0300

    chaufa commit inicial

---
+++ src/camry.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#define VERSION "0.1v"
+
+void 
+optionhelp() {
+  printf("Uso de Camry: \n");
+  printf("-h --help   Despliega este mensaje. \n");
+  printf("-v --version  Muestra la versión de Camry. \n");
+}
+
+int 
+main(int argc, char *argv[]) {
+
+	int c;
+
+	while (1) {
+
+		int option_index = 0;
+
+		static struct option long_options[] = {
+				{"help"		, no_argument, 0, 'h'},
+				{"version", no_argument, 0, 'v'},
+				{0, 			0, 			0, 				0}
+				};
+  
+		c = getopt_long(argc, argv, "hv",
+									long_options, &option_index);
+	
+		if (c == -1)
+				break;
+
+		switch (c) {
+			case 'h':
+				optionhelp();
+				break;
+			case 'v':
+				printf("Camry Version: %s\n", VERSION);
+				break;
+			case '?':
+				break;
+			default:
+				break;
+		}
+	}
+			
+	if (argc == 1 || optind == 1) {
+      printf("No se encontraron argumentos. (usa --help para ayuda).\n");
+      exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
+	}
+
+}
